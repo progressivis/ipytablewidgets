110923c864c2669c0381ec9b296107c7be48be07
Christian Poli <Christian.Poli@inria.fr>
2023-02-23 11:04:21 +0100

    fix settings playwright

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -3,10 +3,8 @@ var baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 module.exports = {
     ...baseConfig,
     use: {
-    headless: false,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
-    video: 'on-first-retry',
   },
   timeout: 240000,
   retries: 1,
